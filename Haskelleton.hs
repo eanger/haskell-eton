a4850386a51679af4cd0beae9e6a9b28dd591d50
U-APOLLO\Eric <Eric@Apollo.(none)>
2012-11-26 22:37:27 -0500

    first couple. cheated on the first, so I skipped it

--- Haskelleton.hs
+++ Haskelleton.hs
@@ -1,20 +1,19 @@
------------------------------------------------------------------------------
---
--- Module      :  Haskelleton
--- Copyright   :
--- License     :  AllRightsReserved
---
--- Maintainer  :
--- Stability   :
--- Portability :
---
--- |
---
------------------------------------------------------------------------------
-
-module Haskelleton (
-
-) where
+-- Problem 4
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + (myLength xs)
 
 
+-- Problem 3
+elementAt :: [a] -> Int -> a
+elementAt x y = x !! (pred y)
 
+elementAt' (x:_) 1 = x
+elementAt' (_:x) n = elementAt' x (n - 1)
+
+-- Problem 2
+myButLast :: [a] -> a
+myButLast [] = error "too short!"
+myButLast [x] = error "too short!"
+myButLast (x:_:[]) = x
+myButLast (_:xs) = myButLast xs
